1ce8e52e9dd0ce3410881d1a47e92cbbc3229313
Sandeep <krsandeep525@gmail.com>
2019-07-24 21:04:27 +0530

    my shoppingbackend project setup

---
+++ shoppingbackend/src/main/java/net/sandy/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.sandy.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
